6802727ff69e43527763dbcd76dcacc578dd6865
viethoang14 <85613034+viethoang14@users.noreply.github.com>
2022-09-13 21:59:27 +0700

    appmusic

--- app1.js
+++ app1.js
@@ -33,18 +33,46 @@ const app = {
   },
   songs: [
     {
+      name: "Ngày Không Có Em",
+      singer: ["BÙI THỊ PHƯƠNG THẢO"],
+      path: "./music/y2meta.com - NGÀY KHÔNG CÓ EM - ThịnK (Piano Version) (128 kbps).mp3",
+      image:
+        "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
+    },
+    {
+      name: "Vài Câu Nói Khiến Người Thay Đổi",
+      singer: ["BÙI THỊ PHƯƠNG THẢO"],
+      path: "./music/y2meta.com - vaicaunoicokhiennguoithaydoi - GREY D x tlinh _ Official Music Video (128 kbps).mp3",
+      image:
+        "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
+    },
+    {
+      name: "Chuyện Đôi Ta",
+      singer: ["BÙI THỊ PHƯƠNG THẢO"],
+      path: "./music/y2meta.com - Chuyện Đôi Ta (Freak D Lofi Ver.) - Emcee L ft Muộii (128 kbps).mp3",
+      image:
+        "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
+    },
+    {
+      name: "Anh Thề Đấy",
+      singer: ["BÙI THỊ PHƯƠNG THẢO"],
+      path: "./music/y2meta.com - Anh Thề Đấy - Thanh Hưng「Lo - Fi Version by 1 9 6 7」_ Audio Lyrics (128 kbps).mp3",
+      image:
+        "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
+    },
+    {
       name: "Phải Chẳng Em Đã Yêu",
       singer: ["BÙI THỊ PHƯƠNG THẢO"],
       path: "./music/PhaiChangEmDaYeu-JukySanRedT-6940932.mp3",
       image:
-        "https://scontent.fhan3-3.fna.fbcdn.net/v/t1.15752-9/301626946_1527584650990654_6384063224920018234_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_ohc=cqXdgtSHFwAAX-6_sTb&_nc_ht=scontent.fhan3-3.fna&oh=03_AVJ5hioToThBtHZ6ERMXQ5GRo9bjx2qgEa0FtWcOPIQwxg&oe=63388B4D",
+        "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
     },
     {
-      name: "Trên Tình Bạn Dươi Tình Yêu",
+      name: "Trên Tình Bạn Dưới Tình Yêu",
       singer: ["BÙI THỊ PHƯƠNG THẢO"],
       path: "./music/TrenTinhBanDuoiTinhYeu-MIN-6802163.mp3",
       image:
-        "https://scontent.fhph1-1.fna.fbcdn.net/v/t1.15752-9/301070237_1066334854269689_8315997769231671416_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Izi9ZxdAXlkAX-cpi6k&_nc_ht=scontent.fhph1-1.fna&oh=03_AVInyn-tdgO-PyltaPwGZLx3dKXcYp36iGe0ZhRGg73Izw&oe=6339EF17",
+        "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
     },
     {
       name: "Tình Bạn Diệu Kỳ",
@@ -57,13 +85,6 @@ const app = {
       name: "Phía Sau Em",
       singer: ["BÙI THỊ PHƯƠNG THẢO"],
       path: "./music/Phia-Sau-Em-Kay-Tran-Binz.mp3",
-      image:
-        "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
-    },
-    {
-      name: "Dằm Trong Tim",
-      singer: ["BÙI THỊ PHƯƠNG THẢO"],
-      path: "./music/y2meta.com - dằm trong tim - SUNI HẠ LINH & TDK _ ‘Hương Mùa Hè’ show (tập 3) (128 kbps).mp3",
       image:
         "https://scontent.fhph1-3.fna.fbcdn.net/v/t1.15752-9/300819430_747372289692582_5996322599485612626_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=TmaplgNmjvQAX9LUulC&_nc_ht=scontent.fhph1-3.fna&oh=03_AVL5wdPuE4kB5I6fjyYtSZlnWxAq-msya5_MLhNdOUVqyQ&oe=633AEC61",
     },
